a4d1d74c05b61474b57907b5d5da7ab511d1614e
ergimotomtech <ergi@motomtech.com>
2022-12-05 16:43:10 +0100

    test

--- simple-api/server.js
+++ simple-api/server.js
@@ -4,7 +4,7 @@ var app = express();
 require('dotenv').config();
 
 app.get('/', function (req, res) {
-   const envVar = 'Bill test ' + ' ' + process.env.VAR + " " + process.env.DOCKER_IMAGE_VERSION;
+   const envVar = 'Ergi 0.97 test ' + ' ' + process.env.VAR + " " + process.env.DOCKER_IMAGE_VERSION;
    console.log(`envVar: ${envVar}`);
    res.json(envVar);
 })
